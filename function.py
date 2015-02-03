b88f638dc063ad096f3061a7e8ab87cc9c239854
alphaqcode <duset@126.com>
2015-02-03 11:08:47 +0800

    modify by master

--- function.py
+++ function.py
@@ -1,3 +1,4 @@
+#normal function
 def my_abs(x):
     if not isinstance(x,(int,float)):
         raise TypeError('bad operand type')
@@ -6,3 +7,18 @@ def my_abs(x):
     else:
         return -x
 
+#default value
+def power(x,n=2):
+    s = 1
+    while n > 0:
+        n = n -1
+        s = s * x
+    return s
+
+
+#default notice master
+def add_end(L=None):
+    if L is None:
+        L = []
+    L.append('END')
+    return L
